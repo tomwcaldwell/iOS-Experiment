c720f7a9ce2f346f54ac38014866faf7bdb97204
WIND Site <thomascaldwell1@hotmail.co.uk>
2017-05-18 16:22:42 +0100

    Initial Commit

---
+++ ExperimentProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExperimentProject
+//
+//  Created by Thomas W Caldwell on 18/05/2017.
+//  Copyright © 2017 Tom Caldwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
